76035a443fac14d835f49222c4fba05d0a234fd0
kanekotic <alvarojosepl@gmail.com>
2018-09-01 12:27:15 +0200

    added unsafe tagger

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,11 +1,33 @@
 const router = require('express').Router()
 
-const table = {}
+const table = {},
+    unsafeRegex = /(nazi|fascist|porn)/g,
+    template = { 
+        history : [], 
+        preferences: {
+            unsafe: {
+                pages: [],
+                time: 0,
+            }
+        } 
+    }
+
+const unsafeModifier = (req, user) =>
+{
+    if(!unsafeRegex.test(req.body.domain))
+        return
+    req.body.tags.push("unsafe")  
+    if(!user.preferences.unsafe.pages.includes(req.body.domain))
+        user.preferences.unsafe.pages.push(req.body.domain)  
+    user.preferences.unsafe.time += req.body.millisecondsSpent  
+}
 
 router.post('/:id/visits', async (req, res) => {
     let user = table[req.params.id]
     if(!user)
-        table[req.params.id] = { history : [] }
+        user = table[req.params.id] = Object.assign({}, template)
+    req.body.tags = []
+    unsafeModifier(req, user)
     table[req.params.id].history.push(req.body)
     res.send({
         type: 'success'
@@ -16,7 +38,18 @@ router.get('/:id/visits', async (req, res) => {
     let user = table[req.params.id]
     if (!user)
         table[req.params.id] = { history : [] }
-    res.send({ history: table[req.params.id].history })
+    res.send({ 
+        history: table[req.params.id].history 
+    })
+})
+
+router.get('/:id/preferences', async (req, res) => {
+    let user = table[req.params.id]
+    if (!user)
+        user = table[req.params.id] = Object.assign({}, template)
+    res.send({ 
+        preferences: table[req.params.id].preferences 
+    })
 })
 
 module.exports = router
